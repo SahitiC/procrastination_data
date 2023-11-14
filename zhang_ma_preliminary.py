49966ba9f6c386a5343fa455c6b2eff878f33f27
Sahiti Chebolu <schebolu@mpi.localnet>
2023-11-14 17:28:18 +0100

    finalise best cluster conditions, analyse clusters
    best settings so far: k-mans, 8 clusters, soft-dtw distance metric

--- zhang_ma_preliminary.py
+++ zhang_ma_preliminary.py
@@ -108,7 +108,7 @@ for i in range(len(data_relevant)):
     cumulative_normalised.append(temp/data_relevant['Total credits'][i])
 data_relevant['cumulative progress normalised'] = cumulative_normalised
 
-km = TimeSeriesKMeans(n_clusters=7, metric="softdtw")
+km = TimeSeriesKMeans(n_clusters=9, metric="softdtw")
 timseries_to_cluster = np.vstack(
     data_relevant['cumulative progress normalised'])
 labels = km.fit_predict(timseries_to_cluster)
@@ -122,5 +122,19 @@ for label in set(labels):
         if data_relevant['labels'][i] == label:
             # ast.literal_eval(data_relevant['delta progress'][i])
             # data_relevant['cumulative progress normalised'][i]
-            plt.plot(ast.literal_eval(data_relevant['delta progress'][i]),
+            plt.plot(data_relevant['cumulative progress normalised'][i],
                      alpha=0.5)
+
+# what are the total credits for each of the clusters
+fig, ax = plt.subplots()
+scatter = ax.scatter(data_relevant['when hit 7 credits'],
+                     data_relevant['Total credits'],
+                     c=data_relevant['labels'])
+ax.set_xlabel('day when 7 credits are hit')
+ax.set_ylabel('total credits completed')
+legend = ax.legend(*scatter.legend_elements(),
+                   loc="center right",
+                   bbox_to_anchor=(1.5, 0.5),
+                   title="clusters")
+ax.add_artist(legend)
+plt.show()
