398f62848acf577239dccdda4ad215b59f58ff33
Sahiti Chebolu <schebolu@mpi.localnet>
2023-12-08 14:47:07 +0100

    create function module for task structure definitions

--- basic_model.py
+++ basic_model.py
@@ -1,5 +1,4 @@
-from scipy.stats import binom
-import seaborn as sns
+
 import mdp_algms
 import task_structure
 import matplotlib.pyplot as plt
@@ -45,146 +44,42 @@ for state_current in range(STATES_NO):
 # allow as many units as possible based on state
 ACTIONS = [np.arange(STATES_NO-i) for i in range(STATES_NO)]
 
-HORIZON = 110  # no. of weeks for task
+HORIZON = 16  # no. of weeks for task
 DISCOUNT_FACTOR = 0.9  # discounting factor
-EFFICACY = 0.5  # self-efficacy (probability of progress for each unit)
+EFFICACY = 0.9  # self-efficacy (probability of progress for each unit)
 
 # utilities :
-REWARD_THR = 20000.0  # reward per unit at threshold (14 units)
+REWARD_THR = 4.0  # reward per unit at threshold (14 units)
 REWARD_EXTRA = REWARD_THR/4  # reward per unit after threshold upto 22 units
 REWARD_SHIRK = 0.1
 EFFORT_WORK = -0.3
 
 # %%
 
-# self paced (immediate) course rewards
-reward_func = []
-for state_current in range(len(STATES)):
+reward_func = task_structure.reward_no_immediate(STATES, ACTIONS, REWARD_SHIRK)
 
-    reward_temp = np.zeros((len(ACTIONS[state_current]), len(STATES)))
+effort_func = task_structure.effort(STATES, ACTIONS, EFFORT_WORK)
 
-    for action in range(len(ACTIONS[state_current])):
+total_reward_func_last = task_structure.reward_final(STATES, REWARD_THR,
+                                                     REWARD_EXTRA)
 
-        reward_temp[action, 0:state_current+action+1] = ((len(STATES)-1-action)
-                                                         * REWARD_SHIRK)
-
-    if state_current < 14:
-
-        for i, action in enumerate(ACTIONS[state_current]
-                                   [14-state_current:22-state_current+1]):
-
-            reward_temp[action, 14:action+state_current+1] += (
-                14*REWARD_THR
-                + np.arange(0, action+state_current+1-14, step=1)*REWARD_EXTRA)
-
-        for i, action in enumerate(ACTIONS[state_current]
-                                   [22-state_current+1:]):
-
-            reward_temp[action, 14:23] += np.arange(14*REWARD_THR,
-                                                    16.25*REWARD_THR,
-                                                    step=REWARD_THR/4)
-            reward_temp[action, 23:action+state_current+1] += 16*REWARD_THR
-
-    elif state_current >= 14 and state_current < 22:
-
-        for i, action in enumerate(ACTIONS[state_current]
-                                   [:22-state_current+1]):
-
-            reward_temp[action, state_current+1: action+state_current+1] += (
-                np.arange(1, action+1)*REWARD_THR/4)
-
-       # reward_temp[22-state_current+1:, :] = reward_temp[22-state_current, :]
-
-    reward_func.append(reward_temp)
-
-# delayed rewards for final course rewards
-reward_func = []
-for state_current in range(len(STATES)):
-
-    reward_temp = np.zeros((len(ACTIONS[state_current]), len(STATES)))
-
-    for action in range(len(ACTIONS[state_current])):
-
-        reward_temp[action, 0:state_current+action+1] = ((len(STATES)-1-action)
-                                                         * REWARD_SHIRK)
-
-    reward_func.append(reward_temp)
-
-effort_func = []
-for state_current in range(len(STATES)):
-
-    effort_temp = np.zeros((len(ACTIONS[state_current]), len(STATES)))
-
-    for i, action in enumerate(ACTIONS[state_current]):
-
-        effort_temp[action, :] = action * EFFORT_WORK
-
-    effort_func.append(effort_temp)
-
-total_reward_func_last = np.zeros(len(STATES))
-# np.zeros(len(STATES))
-# np.arange(0, STATES_NO, 1)*REWARD_THR
-total_reward_func_last[14:22+1] = (14*REWARD_THR
-                                   + np.arange(0, 22-14+1)*REWARD_EXTRA)
-total_reward_func_last[23:] = 14*REWARD_THR + 8*REWARD_EXTRA
-
+# total reward= reward+effort
 total_reward_func = []
 for state_current in range(len(STATES)):
 
     total_reward_func.append(reward_func[state_current]
                              + effort_func[state_current])
 
-T = []
-for state_current in range(len(STATES)):
-
-    T_temp = np.zeros((len(ACTIONS[state_current]), len(STATES)))
-
-    for i, action in enumerate(ACTIONS[state_current]):
-
-        T_temp[action, state_current:state_current+action+1] = (
-            binom(action, EFFICACY).pmf(np.arange(action+1))
-        )
-
-    T.append(T_temp)
-
-# transition functions with decreasing efficacy
-T = []
-for i_timestep in range(HORIZON):
-    T_t = []
-    efficacy = EFFICACY * (1 - (i_timestep / HORIZON))
-
-    for state_current in range(len(STATES)):
-
-        T_temp = np.zeros((len(ACTIONS[state_current]), len(STATES)))
-
-        for i, action in enumerate(ACTIONS[state_current]):
-
-            T_temp[action, state_current:state_current+action+1] = (
-                binom(action, efficacy).pmf(np.arange(action+1))
-            )
-
-        T_t.append(T_temp)
-    T.append(T_t)
-
 # %%
+
+T = task_structure.T_binomial(STATES, ACTIONS, EFFICACY)
 
 V_opt, policy_opt, Q_values = mdp_algms.find_optimal_policy_prob_rewards(
     STATES, ACTIONS, HORIZON, DISCOUNT_FACTOR,
     total_reward_func, total_reward_func_last, T)
 
 efficacy_actual = EFFICACY
-T_actual = []
-for state_current in range(len(STATES)):
-
-    T_temp = np.zeros((len(ACTIONS[state_current]), len(STATES)))
-
-    for i, action in enumerate(ACTIONS[state_current]):
-
-        T_temp[action, state_current:state_current+action+1] = (
-            binom(action, efficacy_actual).pmf(np.arange(action+1))
-        )
-
-    T_actual.append(T_temp)
+T_actual = task_structure.T_binomial(STATES, ACTIONS, efficacy_actual)
 
 initial_state = 0
 s, a, v = mdp_algms.forward_runs(
@@ -206,6 +101,8 @@ plt.xlabel('weeks')
 plt.ylabel('units completed')
 
 # %%
+
+T = task_structure.T_binomial_decreasing(STATES, ACTIONS, HORIZON, EFFICACY)
 
 V_opt, policy_opt, Q_values = mdp_algms.find_optimal_policy_T_time_dep(
     STATES, ACTIONS, HORIZON, DISCOUNT_FACTOR,
